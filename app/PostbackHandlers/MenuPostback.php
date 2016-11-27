fb38041582671e9541e03958330c9effacae7bf6
KID <jyhsu2000@yahoo.com.tw>
2016-11-28 00:32:16 +0800

    Update MenuPostback

--- app/PostbackHandlers/MenuPostback.php
+++ app/PostbackHandlers/MenuPostback.php
@@ -3,6 +3,8 @@
 namespace App\PostbackHandlers;
 
 use Casperlaitw\LaravelFbMessenger\Contracts\PostbackHandler;
+use Casperlaitw\LaravelFbMessenger\Messages\ButtonTemplate;
+use Casperlaitw\LaravelFbMessenger\Messages\GenericTemplate;
 use Casperlaitw\LaravelFbMessenger\Messages\ReceiveMessage;
 use Casperlaitw\LaravelFbMessenger\Messages\Text;
 
@@ -21,6 +23,30 @@ class MenuPostback extends PostbackHandler
     public function handle(ReceiveMessage $message)
     {
         //TODO: 顯示選單
-        $this->send(new Text($message->getSender(), "I got your payload"));
+        $button = new ButtonTemplate($message->getSender(), 'Default text');
+        $button->setText('Choose')
+            ->addPostBackButton('First Bbutton')
+            ->addPostBackButton('Second Button')
+            ->addPostBackButton('Third button');
+        $this->send($button);
+
+        $generic = new GenericTemplate($message->getSender());
+        $generic->addElement('First item', 'description')
+            ->buttons()
+            ->addPostBackButton('First Button')
+            ->addWebButton('facebook', 'https://facebook.com');
+        $generic->addElement('Second item', 'description')
+            ->buttons()
+            ->addPostBackButton('Second Button')
+            ->addWebButton('google', 'http://www.google.com')
+            ->addShareButton();
+        $this->send($generic);
+
+        $generic = new GenericTemplate($message->getSender());
+        $generic->addElement('First item', 'description')
+            ->buttons()
+            ->addPostBackButton('First Button')
+            ->addAccountLinkButton('My auth url');
+        $this->send($generic);
     }
 }
