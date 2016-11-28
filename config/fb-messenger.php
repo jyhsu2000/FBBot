5c6769aeec902961fc58351856ceecb1a58c7226
KID <jyhsu2000@yahoo.com.tw>
2016-11-28 14:24:20 +0800

    Use MESSENGER_DEBUG for fb bot debug toggle

--- config/fb-messenger.php
+++ config/fb-messenger.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'debug'        => env('APP_DEBUG', false),
+    'debug'        => env('MESSENGER_DEBUG', false),
     'verify_token' => env('MESSENGER_VERIFY_TOKEN'),
     'app_token'    => env('MESSENGER_APP_TOKEN'),
     'auto_typing'  => true,
