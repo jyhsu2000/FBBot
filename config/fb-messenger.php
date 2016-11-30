cd8eeb54823abe5b1029409dc4966c598d32103a
KID <jyhsu2000@yahoo.com.tw>
2016-11-30 13:57:00 +0800

    Turn off auto typing

--- config/fb-messenger.php
+++ config/fb-messenger.php
@@ -4,7 +4,7 @@ return [
     'debug'        => env('MESSENGER_DEBUG', false),
     'verify_token' => env('MESSENGER_VERIFY_TOKEN'),
     'app_token'    => env('MESSENGER_APP_TOKEN'),
-    'auto_typing'  => true,
+    'auto_typing'  => false,
     'handlers'     => [
         App\Chatbot\MessageHandlers\DefaultHandler::class,
     ],
