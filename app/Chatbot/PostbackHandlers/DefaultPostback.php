4f1da5a870b397bb5d96234d7467a86c18a40141
KID <jyhsu2000@yahoo.com.tw>
2016-12-03 22:25:46 +0800

    Create DefaultPostback

---
+++ app/Chatbot/PostbackHandlers/DefaultPostback.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Chatbot\PostbackHandlers;
+
+use Casperlaitw\LaravelFbMessenger\Contracts\PostbackHandler;
+use Casperlaitw\LaravelFbMessenger\Messages\ReceiveMessage;
+use Casperlaitw\LaravelFbMessenger\Messages\Text;
+
+class DefaultPostback extends PostbackHandler
+{
+    // If webhook get the $payload is `USER_DEFINED_PAYLOAD` will run this postback handler
+    protected $payload = ''; // You also can use regex!
+
+    /**
+     * Handle the chatbot message
+     *
+     * @param ReceiveMessage $message
+     *
+     * @return mixed
+     */
+    public function handle(ReceiveMessage $message)
+    {
+        $postback = $message->getPostback();
+
+        $text = new Text($message->getSender(), 'Postback: ' . $postback);
+        $this->send($text);
+    }
+}
