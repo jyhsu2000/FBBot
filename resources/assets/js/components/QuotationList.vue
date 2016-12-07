ee21a746b81df12bb8fe3373090bcf6d7be467bb
KID <jyhsu2000@yahoo.com.tw>
2016-12-07 13:39:06 +0800

    Update QuotationList

--- resources/assets/js/components/QuotationList.vue
+++ resources/assets/js/components/QuotationList.vue
@@ -7,19 +7,22 @@
 <script>
     export default {
         ready() {
-            console.log('QuotationList ready.');
             this.$nextTick(function () {
                 this.fetch();
             });
         },
-        props: ['quotations', 'api'],
+        data: function () {
+            return {
+                quotations: []
+            }
+        },
+        props: ['api'],
         methods: {
             fetch: function () {
                 this.$http.get(this.api).then(function (response) {
                     var json = response.json();
                     var quotations = [];
                     $.each(json, function (key, value) {
-                        console.log(value);
                         quotations.push(value);
                     });
                     this.quotations = quotations;
