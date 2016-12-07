626b74e20f4abcadf9bd52e8935ad2121fc9da59
KID <jyhsu2000@yahoo.com.tw>
2016-12-07 17:25:20 +0800

    Create player

---
+++ database/migrations/2016_12_07_172048_create_players_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('app_uid')->unique();
+            $table->string('uid')->nullable();
+            $table->string('nid')->nullable();
+            $table->integer('in_question')->nullable();
+            $table->integer('time')->default(0);
+            $table->string('state')->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
