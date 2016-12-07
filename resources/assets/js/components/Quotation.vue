27c1696b933712e070ee57f14dd6c7bb5eb20f55
KID <jyhsu2000@yahoo.com.tw>
2016-12-07 21:54:02 +0800

    Update Quotation.vue

--- resources/assets/js/components/Quotation.vue
+++ resources/assets/js/components/Quotation.vue
@@ -1,11 +1,17 @@
 <template>
-    <li class="list-group-item">
-        <span class="tag tag-default">#{{ quotation.id }}</span>
-        <span class="tag tag-primary">{{ quotation.counter }}</span>
-        {{ quotation.content }}
-        <button class="btn btn-danger float-sm-right" @click="$parent.destroy(quotation)">
-            <i class="fa fa-trash-o" aria-hidden="true"></i> 刪除
-        </button>
+    <li class="list-group-item row">
+        <div class="col-sm-1" style="white-space: nowrap">
+            <span class="tag tag-default">#{{ quotation.id }}</span>
+            <span class="tag tag-primary">{{ quotation.counter }}</span>
+        </div>
+        <div class="col-sm-10">
+            {{ quotation.content }}
+        </div>
+        <div class="col-sm-1">
+            <button class="btn btn-danger float-sm-right" @click="$parent.destroy(quotation)">
+                <i class="fa fa-trash-o" aria-hidden="true"></i> 刪除
+            </button>
+        </div>
     </li>
 </template>
 
@@ -15,4 +21,7 @@
             'quotation'
         ]
     }
+
+
+
 </script>
