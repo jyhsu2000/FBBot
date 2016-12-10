7cbf5885517f1a495f748c9941199e635a308c3a
KID <jyhsu2000@yahoo.com.tw>
2016-12-11 02:34:03 +0800

    Update for test

--- app/Chatbot/Postbacks/ChallengePostback.php
+++ app/Chatbot/Postbacks/ChallengePostback.php
@@ -44,7 +44,7 @@ class ChallengePostback extends Postback
         } elseif ($action == 'ANSWER') {
             //點擊答案
             //TODO: 觸發作答任務
-            $text = new Text($sender, 'data: ' . $data);
+            $text = new Text($sender, 'data: ' . json_encode($data));
             $handler->send($text);
 
             return;
