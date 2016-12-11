d5bcaf469e71897207076e977c2c8d7f1f8623df
KID <jyhsu2000@yahoo.com.tw>
2016-12-11 22:31:48 +0800

    Update phpDoc

--- app/Qualification.php
+++ app/Qualification.php
@@ -4,6 +4,22 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Qualification
+ *
+ * @property int $id
+ * @property int $player_id
+ * @property \Carbon\Carbon $get_at
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property-read \App\Player $player
+ * @method static \Illuminate\Database\Query\Builder|\App\Qualification whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Qualification wherePlayerId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Qualification whereGetAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Qualification whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Qualification whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Qualification extends Model
 {
     /**
