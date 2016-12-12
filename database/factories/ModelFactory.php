5fd020f381f0860b8a0967ff03ab5216ecadd3e7
KID <jyhsu2000@yahoo.com.tw>
2016-12-12 15:08:27 +0800

    Create QualificationSeeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -30,9 +30,9 @@ $factory->define(App\Quotation::class, function (Faker\Generator $faker) {
 
 $factory->define(App\Player::class, function (Faker\Generator $faker) {
     return [
-        'app_uid'  => $faker->unique()->randomNumber,
-        'nid'      => $faker->optional()->regexify('[DEPMV]{1}[0-9]{7}'),
-        'uuid'     => $faker->uuid,
+        'app_uid' => $faker->unique()->randomNumber,
+        'nid'     => $faker->optional()->regexify('[DEPMV]{1}[0-9]{7}'),
+        'uuid'    => $faker->uuid,
     ];
 });
 
@@ -43,9 +43,18 @@ $factory->define(App\Question::class, function (Faker\Generator $faker) {
         'wrong_message'   => $faker->optional()->sentence,
     ];
 });
+
 $factory->define(App\Choice::class, function (Faker\Generator $faker) {
     return [
         'content'    => $faker->sentence,
         'is_correct' => $faker->boolean,
+    ];
+});
+
+$factory->define(App\Qualification::class, function (Faker\Generator $faker) {
+    return [
+        'player_id'  => $faker->randomElement(App\Player::pluck('id')->toArray()),
+        'get_at'     => $faker->optional()->dateTimeBetween('-2 days'),
+        'created_at' => $faker->dateTimeBetween('-10 days', '-2 days'),
     ];
 });
---
+++ database/seeds/QualificationSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class QualificationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Qualification::class, 50)->create();
+    }
+}
