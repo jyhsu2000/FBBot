1293612a1d023f6a60a6e314cde9ad4c4c9aebd1
KID <jyhsu2000@yahoo.com.tw>
2016-12-14 02:34:22 +0800

    Fix choice content length limit

--- app/Http/Controllers/ChoiceController.php
+++ app/Http/Controllers/ChoiceController.php
@@ -70,7 +70,7 @@ class ChoiceController extends Controller
     public function update(Request $request, Choice $choice)
     {
         $validator = \Validator::make($request->all(), [
-            'content' => 'required|max:255',
+            'content' => 'required|max:20',
         ]);
         if ($validator->fails()) {
             return response()->json([
