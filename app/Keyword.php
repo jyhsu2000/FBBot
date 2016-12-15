319435e8c86a17f27b5dc1e5d0fb5899275395b5
KID <jyhsu2000@yahoo.com.tw>
2016-12-15 19:14:27 +0800

    Update phpDoc

--- app/Keyword.php
+++ app/Keyword.php
@@ -4,6 +4,21 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Keyword
+ *
+ * @property int $id
+ * @property string $keyword
+ * @property string $reply
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Keyword whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Keyword whereKeyword($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Keyword whereReply($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Keyword whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Keyword whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Keyword extends Model
 {
     protected $fillable = [
