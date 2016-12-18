e44bf509e47136a60ecbf002684df21bfc12cb9d
KID <jyhsu2000@yahoo.com.tw>
2016-12-18 17:14:59 +0800

    Remove URL handling

--- app/Chatbot/MessageHandlers/DefaultHandler.php
+++ app/Chatbot/MessageHandlers/DefaultHandler.php
@@ -58,23 +58,11 @@ class DefaultHandler extends BaseHandler
                 /* @var AutoReplyMessage $autoReplyMessage */
                 $autoReplyMessage = $keyword->autoReply->autoReplyMessages->random(1);
                 $content = $autoReplyMessage->content;
-                //處理網址
-                $urlPattern = '/(?:http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(?:\/\S*)?/';
-                $matchCount = preg_match_all($urlPattern, $content, $matches);
-                $linkButtonCount = min($matchCount, 3);
-                //連結按鈕
-                $buttonTemplate = new ButtonTemplate($sender);
-                $buttonTemplate->setText('Links:');
-                for ($i = 0; $i < $linkButtonCount; $i++) {
-                    $buttonTemplate->addWebButton($matches[0][$i], $matches[0][$i]);
-                }
                 //計算次數
                 $keyword->increment('counter');
                 $autoReplyMessage->increment('counter');
                 //傳送訊息
                 $this->send(new Text($sender, $content));
-                //傳送按鈕
-                $this->send($buttonTemplate);
 
                 return;
             }
