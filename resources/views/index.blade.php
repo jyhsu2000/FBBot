304573358ab09960ce36721d047a8b4136e6488f
KID <jyhsu2000@yahoo.com.tw>
2016-12-19 20:25:53 +0800

    Update home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,8 +19,9 @@
         <h1 class="display-1">{{ config('app.name') }}</h1>
         <h2 class="display-3">資訊安全宣導週</h2>
         <h3 class="display-4">資安大挑戰</h3>
-        <a href="http://m.me/fcu.test" class="btn btn-primary btn-lg" style="margin: 30px;"
-           title="Let's GO!!">GO!</a>
+        <a href="http://m.me/fcu.test" class="btn btn-primary btn-lg" style="margin-top: 30px;"
+           title="Let's GO!!">GO!</a><br/>
+        ↑按下按鈕開始挑戰↑
         <div>
             <img src="{{ asset('img/tutorial.png') }}" alt="簡易說明" class="img-thumbnail">
         </div>
