76fa1083521eb2d7c15fbc21ade93c1d59e4f57f
Chan, Danny <danny50610@yahoo.com.tw>
2016-12-20 01:05:06 +0800

    Change home page layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,25 +5,26 @@
 @section('css')
     <style>
         .jumbotron {
-            text-align: center;
             word-break: break-all;
-            background: rgba(100, 100, 100, .6);
-            margin-top: 10vh;
-            padding-top: 40px;
+            background: rgba(131, 131, 131, 0.6);
             border-radius: 20px;
+            min-height: 80vh;
         }
     </style>
 @endsection
 @section('content')
-    <div class="jumbotron">
-        <h1 class="display-1">{{ config('app.name') }}</h1>
-        <h2 class="display-3">資訊安全宣導週</h2>
-        <h3 class="display-4">資安大挑戰</h3>
-        <a href="http://m.me/fcu.test" class="btn btn-primary btn-lg" style="margin-top: 30px;"
-           title="Let's GO!!">GO!</a><br/>
-        ↑按下按鈕開始挑戰↑
-        <div>
-            <img src="{{ asset('img/tutorial.png') }}" alt="簡易說明" class="img-thumbnail">
+    <div class="jumbotron" style="padding-top: 15vh;">
+        <div class="float-lg-left" style="margin-bottom: 5vh;">
+            <h1 class="display-3">資訊安全宣導週</h1>
+            <h1 class="display-4">資安大挑戰</h1>
+            <div style="margin-top: 30px;">
+                <a href="http://m.me/fcu.test" class="btn btn-primary btn-lg" title="Let's GO!!"
+                   style="font-size: 2.5rem;">GO!</a>
+                <span style="font-size: 2rem;" class="align-middle"><i class="fa fa-arrow-left" aria-hidden="true"></i>開始挑戰</span>
+            </div>
+        </div>
+        <div class="text-xs-center">
+            <img src="{{ asset('img/tutorial.png') }}" alt="簡易說明" class="img-thumbnail float-lg-right">
         </div>
     </div>
 @endsection
